structure is mostly finished but y_hat calculation needs to be fixed

multiclass_perceptron.py
@@ -15,14 +15,19 @@ learning() function
 
 returns: 1 for class 1 and -1 for class 2
 """
-def activation(x, w, bias=0):
-	x=_prepend_test_feature(x)
-	print("\n\n")
-	print(str(x))
-	print(str(w))
-	if(np.dot(x,w)-bias>0):
-		return 1
-	return -1
+def testset_activation(fname, w, num_labels, start_label, bias=0):
+	data=load_svmlight_file(fname)
+	num_samples=data[0].get_shape()[0]
+	print("num samples: " + str(num_samples))
+	for i in range(num_samples):
+		xi=np.insert(data[0].getrow(i).toarray()[0],0,1)
+		current_label=start_label
+		for j in range(w.shape[0]):
+			y_hat=np.dot(xi,w[j])
+			if(y_hat<=current_label):
+				print(current_label)
+				break
+			current_label+=1
 	
 
 """
@@ -31,7 +36,7 @@ x is the training set which is an NxD matrix
 y is the 1xD set of labels corresponding to the training set
 y is either 1 or -1
 
-limit is factor of 10000 for multiples of training iterations.
+limit is factor of 100 for multiples of training iterations.
 limit=1 means there will be 10000 iterations, 2 will be 2000 and so on
 This is used because the training process doesn't converge for non-linearly
 separable data
@@ -40,54 +45,37 @@ step size is a constant used for updating the weights
 
 returns: weight vector to use as input on activation function
 """
-def learning(fname, limit,num_classes, step_size, bias=0):
+def learning(fname, limit,num_labels, start_label, step_size, bias=0):
 	data=load_svmlight_file(fname)
 	samples=data[0].get_shape()[0]
 	features=data[0].get_shape()[1]
-	
-		
-	for j in range(num_classes-1):#need to do more with this
+	current_label=start_label
+	for j in range(num_labels-1):#need to do more with this
 		limit_count=0
-		while limit_count < limit*10000:
+		temp_w=np.ones(features+1)
+		while limit_count < limit*1000:
 			y_hat=0
 			count=0	#counts the amount of updates 
-			for i in range(x.shape[0]):
-				y_hat=np.dot(w,x[i])
-				if y_hat-bias > 0:
-					y_hat=1
-				else:
-					y_hat=-1
-				if not y[i] == y_hat:
-					w=w+step_size*y[i]*x[i]
-					count+=1
-			if count==0:#check if w has converged
-				print("converged!")
+			for i in range(samples):
+				if data[1][i] >= current_label:
+					xi=np.insert(data[0].getrow(i).toarray()[0],0,1)
+					y_hat=np.dot(temp_w,xi)
+					if y_hat-bias > 0:
+						y_hat=1
+					else:
+						y_hat=-1
+					if not y_hat == 1:	#data[1][i] is the label
+						temp_w=temp_w+step_size*data[1][i]*xi
+						count+=1
+			if count==0:#check if w has converged and break if yes
+				print("weight vector " + str(j) + " converged!")
 				break
 			limit_count+=1
+		current_label+=1
 		if limit_count == limit*1000:
-			print("did not converge")
-		return w
-
-
-"""
-this is used to prepend a column of 1s to the training set
-in order to to avoid using a separate bias. 
-
-only use this on the training data
-
-x is the input data in the form of a numpy array. 
-Can be either 1D or 2D. if 1D this function will convert it
-"""
-def _prepend_trng_feature(x):
-	a=np.ones((x.shape[0],1))
-	print(str(x.ndim)+ " dimensions")
-	if x.ndim > 1:
-		return np.hstack((a,x))
-	else:
-		return np.hstack((a,np.array(x)[np.newaxis].T))
-
-def _prepend_test_feature(x):
-	return np.insert(x, 0, 1.,axis=0)
-
-
-
+			print("weight vector " + str(j) + " did not converge")
+		if j == 0:
+			w=np.array([temp_w])
+		else:
+			w=np.vstack((w, temp_w))
+	return w